4f283c679c2abd1d14ec3732c78d757dd9c9007d
Eddy Nguyen <eddeee.nguyen@gmail.com>
2018-08-24 19:37:55 +1000

    Fix page default loading spinner

--- application/client/src/common/components/loading_placeholders/_page.js
+++ application/client/src/common/components/loading_placeholders/_page.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Page = () => <div>Loading...</div>;
+const StyledWrapper = styled.div`
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const Page = () => <StyledWrapper>Loading...</StyledWrapper>;
 
 export default Page;
